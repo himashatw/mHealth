2929cca8b12a5aaa7a37b24fc3d27648e74f85bb
Himasha Weerasinghe <himashatw@gmail.com>
2022-07-09 23:35:13 +0530

    emoji face component added

---
+++ lib/components/emoticon_face.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class EmoticonFace extends StatelessWidget {
+  final String emoticonFace;
+  const EmoticonFace({Key? key, required this.emoticonFace}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: Colors.blue[600],
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Center(
+        child: Text(
+          emoticonFace,
+          style: TextStyle(
+            fontSize: 28,
+          ),
+        ),
+      ),
+    );
+  }
+}
